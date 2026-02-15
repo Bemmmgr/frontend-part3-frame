06046628e168e21939b243968ea57c43574c51f1
Xiangyu Li <lix0230@gmail.com>
2026-02-15 16:23:32 +0100

    learn basic concept from DateCounter, setting up for quiz app

--- react-myPractice/10-react-quiz/src/App.js
+++ react-myPractice/10-react-quiz/src/App.js
@@ -1,9 +1,17 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+// 16006 - React quiz app
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      {/* <DateCounter /> */}
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
--- react-myPractice/10-react-quiz/src/DateCounter.js
+++ react-myPractice/10-react-quiz/src/DateCounter.js
@@ -1,8 +1,44 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+/*
+不会直接 setState，而是 dispatch(action)（发出“发生了什么事”）
+React 会调用 reducer(state, action) 计算 nextState
+如果 nextState !== state（引用变化），组件 re-render
+*/
+const initialState = { count: 0, step: 1 };
+
+// 16002 - another hook useReducer
+// 16003 - Manage related pieces of state
+function reducer(state, action) {
+  console.log(state, action);
+  // return state + action; // become new state
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "dec") return state - 1;
+  // if (action.type === "setCount") return action.payload;
+
+  switch (action.type) {
+    case "inc":
+      // 在不破坏原 state 的前提下（不可变更新），把 count 加 1
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("Unknow action");
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -10,25 +46,30 @@ function DateCounter() {
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    // setCount(0);
+    // setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
