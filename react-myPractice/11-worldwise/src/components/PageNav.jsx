0f847f64f0dcca4bc65ab362c16420be132bbef2
Xiangyu Li <lix0230@gmail.com>
2026-02-21 15:51:59 +0100

    finish pageNav for each page

--- react-myPractice/11-worldwise/src/components/PageNav.jsx
+++ react-myPractice/11-worldwise/src/components/PageNav.jsx
@@ -2,19 +2,23 @@ import { NavLink } from "react-router-dom";
 
 // 17007 - using CSS modules: create one external CSS file per component
 import styles from "./PageNav.module.css";
+import Logo from "../components/Logo";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Homepage</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
