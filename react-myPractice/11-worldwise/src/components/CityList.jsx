3d2ef8ae253ae5924b0006ce2bd540df3c74b601
Xiangyu Li <lix0230@gmail.com>
2026-02-21 20:57:32 +0100

    implementing cities & countries (sidebar components) of app page

---
+++ react-myPractice/11-worldwise/src/components/CityList.jsx
@@ -0,0 +1,24 @@
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
+// 17011 - implementing the city list
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clikcing on the city on the map" />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
---
+++ react-myPractice/11-worldwise/src/components/CountryList.jsx
@@ -0,0 +1,30 @@
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
+// 17011 - implementing the city list
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clikcing on the city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
