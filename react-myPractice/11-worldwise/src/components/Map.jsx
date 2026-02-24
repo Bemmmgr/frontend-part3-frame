e83c5923028922e87f2ee0ef0a98c7d44a2fd2df
Xiangyu Li <lix0230@gmail.com>
2026-02-24 18:47:12 +0100

    introduce leaflet package, fix Marker display issues

--- react-myPractice/11-worldwise/src/components/Map.jsx
+++ react-myPractice/11-worldwise/src/components/Map.jsx
@@ -1,5 +1,25 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  CircleMarker,
+} from "react-leaflet";
+import { useState } from "react";
+
+// 修复icon
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const icon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
 
 function Map() {
   // 17016 - prommatic navigation with useNavigates
@@ -10,20 +30,25 @@ function Map() {
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
-  return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        position:{lat}, {lng}
-      </h1>
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: 50 });
-        }}
+  return (
+    <div className={styles.mapContainer}>
+      {/* 18011 - including a map using leaflet library */}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition} icon={icon}>
+          <Popup>A pretty CSS3 popup.</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
