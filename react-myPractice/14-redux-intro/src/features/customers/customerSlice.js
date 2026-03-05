585e7726f97927416e2210ac2604d344a2967514
Xiangyu Li <lix0230@gmail.com>
2026-03-05 19:45:07 +0100

    connecting react to redux, ready to dispatch actions

---
+++ react-myPractice/14-redux-intro/src/features/customers/customerSlice.js
@@ -0,0 +1,41 @@
+// 20007 - Professional redux file structure state slices
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+// 20006 -
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+// 20005 - creating a reducer bank account
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "account/updateName", payload: fullName };
+}
