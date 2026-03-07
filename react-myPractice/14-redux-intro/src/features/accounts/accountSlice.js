d0869ef5c4f2ace7e8eb0b823bb37114cf71e51f
Xiangyu Li <lix0230@gmail.com>
2026-03-07 18:14:44 +0100

    converting code to fit redux toolkit

--- react-myPractice/14-redux-intro/src/features/accounts/accountSlice.js
+++ react-myPractice/14-redux-intro/src/features/accounts/accountSlice.js
@@ -1,11 +1,70 @@
-// 20007 - Professional redux file structure state slices
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export default accountSlice.reducer;
+
+/*
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
@@ -31,9 +90,9 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
-
     case "account/convertingCurrency":
       return { ...state, isLoading: true };
+
     default:
       return state;
   }
@@ -46,6 +105,8 @@ export function deposit(amount, currency) {
   // when return function, react knows its asynchronous action
   // middleware - between dispatching and store
   return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
     // API call
     const res = await fetch(
       `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`,
@@ -74,3 +135,4 @@ export function payLoan() {
     type: "account/payLoan",
   };
 }
+*/
