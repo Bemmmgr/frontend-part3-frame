7ec5ed98e751c47c77773f59d84b48b0a40c7390
Xiangyu Li <lix0230@gmail.com>
2026-03-10 17:13:10 +0100

    Import dependencies and use react-router to establish basic path navigation

--- react-myPractice/15-fast-react-pizza/vite.config.js
+++ react-myPractice/15-fast-react-pizza/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       emitWarning: true,
       emitError: true,
       failOnWarning: false,
-      failOnError: true,
+      failOnError: false,
     }),
   ],
 });
