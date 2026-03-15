ba4c0856e06bb82a12a6777a866c8faccdf85ddd
Xiangyu Li <lix0230@gmail.com>
2026-03-15 16:27:04 +0100

    23007-23010 responsive design

--- react-myPractice/15-fast-react-pizza/src/ui/AppLayout.jsx
+++ react-myPractice/15-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,24 +1,26 @@
-import Header from "./Header";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
   // 22008 - displaying a loading indicator
   const navigation = useNavigation();
   // console.log(navigation);
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
-        {/* outlet render nested route(children) */}
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          {/* outlet render nested route(children) */}
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
