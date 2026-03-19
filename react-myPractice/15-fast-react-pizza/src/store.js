53fce9c431ac226de1f5d77f34276494657392ad
Xiangyu Li <lix0230@gmail.com>
2026-03-19 18:24:46 +0100

    24004-24008 modeling cart state, building cart overview, add/delete cartItem

--- react-myPractice/15-fast-react-pizza/src/store.js
+++ react-myPractice/15-fast-react-pizza/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
 
 // 24002 -
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
