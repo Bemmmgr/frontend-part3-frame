b3bf5bbb8abc5b012d272a29a3a4dc0e495e2fa4
Xiangyu Li <lix0230@gmail.com>
2026-04-27 23:49:40 +0200

    28013 - building a reusable context menu(compound component)

--- react-myPractice/16-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ react-myPractice/16-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import Spinner from "../../ui/Spinner";
 import { useCabins } from "./useCabins";
+import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -27,20 +28,23 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header>
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </Table.Header>
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
 
-      {cabins.map((cabin) => (
-        <CabinRow key={cabin.id} cabin={cabin} />
-      ))}
-    </Table>
+        <Table.Body
+          data={cabins}
+          render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+        />
+      </Table>
+    </Menus>
   );
 }
 
