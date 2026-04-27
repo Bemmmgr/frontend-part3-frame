1bc0a423ccb0b3c4f16a678e259105ed101ad147
Xiangyu Li <lix0230@gmail.com>
2026-04-27 18:03:03 +0200

    under compound components, 28009-28011 detecting outside click / confirm deletions / building reusable tables

--- react-myPractice/16-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ react-myPractice/16-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import Modal from "../../ui/Modal";
 import Button from "../../ui/Button";
-import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
 
 // 28007 - building a modal window using react portal
@@ -27,22 +25,16 @@ function AddCabin() {
 // 28008 - converting a modal to a compond component
 function AddCabin() {
   return (
-    // multiple windows
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
