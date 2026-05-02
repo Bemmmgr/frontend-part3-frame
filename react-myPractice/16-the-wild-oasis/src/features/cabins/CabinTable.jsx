9c907be136917cc903e6ad5460b5876929173dad
Xiangyu Li <lix0230@gmail.com>
2026-05-02 23:07:55 +0200

    29002-003 filter and sort

--- react-myPractice/16-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ react-myPractice/16-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -13,6 +13,7 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // 1) Filter
   const filterValue = searchParams.get("discount") || "all";
 
   let filterCabins;
@@ -21,6 +22,14 @@ function CabinTable() {
     filterCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filterCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2) Sort - 29003
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filterCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier,
+  );
 
   return (
     <Menus>
@@ -36,7 +45,8 @@ function CabinTable() {
 
         <Table.Body
           // data={cabins}
-          data={filterCabins}
+          // data={filterCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
