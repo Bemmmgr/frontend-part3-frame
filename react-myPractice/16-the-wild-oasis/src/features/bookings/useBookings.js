d3c53c312e7c672ab56e484cd511d3fba448c400
Xiangyu Li <lix0230@gmail.com>
2026-05-04 20:59:46 +0200

    29005006 uploading samle data & api-side filtering booking

--- react-myPractice/16-the-wild-oasis/src/features/bookings/useBookings.js
+++ react-myPractice/16-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,15 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  // 29006 - API side Filtering booking
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filtervalue = searchParams.get("status");
+  const filter =
+    !filtervalue || filtervalue === "all"
+      ? null
+      : { field: "status", value: filtervalue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
+    queryKey: ["bookings", filter],
     // query func: responsible for actual querying: fetch data from api
-    queryFn: getBookings,
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, error };
--- react-myPractice/16-the-wild-oasis/src/services/apiBookings.js
+++ react-myPractice/16-the-wild-oasis/src/services/apiBookings.js
@@ -2,12 +2,17 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 // 29004 - building bookings table
-export async function getBookings(params) {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)",
     );
+
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
