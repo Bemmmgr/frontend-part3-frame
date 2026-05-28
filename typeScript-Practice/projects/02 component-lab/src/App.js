d679c435db51ab9543d017991d96a12f950bea57
Xiangyu Li <lix0230@gmail.com>
2026-05-28 21:33:46 +0200

    setting up new project for playground of advanced components

---
+++ typeScript-Practice/projects/02 component-lab/src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+function App() {
+    return _jsx("h1", { children: "Let's get started!" });
+}
+export default App;
+//# sourceMappingURL=App.js.map
