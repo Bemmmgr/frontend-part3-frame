88f3d27f366ca63b739ee12c2f6a8e048ec48351
Xiangyu Li <lix0230@gmail.com>
2026-05-31 20:47:18 +0200

    finish timer section with last useEffect and dependencies handle, setting up blog project

--- typeScript-Practice/projects/03 timer-board/src/components/Timer.tsx
+++ typeScript-Practice/projects/03 timer-board/src/components/Timer.tsx
@@ -1,12 +1,48 @@
 import Container from "./UI/Container.tsx";
-import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context.tsx";
+import { useEffect, useRef, useState } from "react";
 
 // 073 -
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  // 075 - creating first side effect
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  // 078 - useEffect & its dependencies
+  const { isRunning } = useTimersContext();
+
+  // 077 - managing interval with Refs & cleanup func
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  // 076 - using useEffect
+  useEffect(() => {
+    let timer: number;
+
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => prevTime - 50);
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
