65d25c4f8e9e63befd10bffa39900266893307b6
Xiangyu Li <lix0230@gmail.com>
2026-06-01 14:47:34 +0200

    finish data fetching & transforming setting up for redux project

---
+++ typeScript-Practice/projects/04 blog-data-fetching/src/utils/http.ts
@@ -0,0 +1,11 @@
+// 081 - building utility get func
+export async function get(url: string) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data!");
+  }
+
+  const data = (await response.json()) as unknown;
+  return data;
+}
